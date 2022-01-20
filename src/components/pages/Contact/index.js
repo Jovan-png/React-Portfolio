304d8dec5f69a79abdcfb40bcb4b206aed6353a5
Jovan-png <88255194+Jovan-png@users.noreply.github.com>
2022-01-19 21:21:09 -0600

    added project cards and contact section

--- src/components/pages/Contact/index.js
+++ src/components/pages/Contact/index.js
@@ -1,20 +1,61 @@
 import React, { useState } from 'react';
 
+import { validateEmail } from '../../../utils/helpers';
 
 function ContactForm() {
+    const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+
+    const [errorMessage, setErrorMessage] = useState('');
+    const { name, email, message } = formState;
+  
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      if (!errorMessage) {
+        setFormState({ [e.target.name]: e.target.value });
+        console.log('Form', formState);
+      }
+    };
+  
+    const handleChange = (e) => {
+      if (e.target.name === 'email') {
+        const isValid = validateEmail(e.target.value);
+        if (!isValid) {
+          setErrorMessage('Your email is invalid.');
+        } else {
+          setErrorMessage('');
+        }
+      } else {
+        if (!e.target.value.length) {
+          setErrorMessage(`${e.target.name} is required.`);
+        } else {
+          setErrorMessage('');
+        }
+      }
+    };
 return(
-    <div className="about-container">
-    <h1>Contact Me</h1>
-    <center>
-   <div className="about-p">
-    <p> My my full name is, Jovan Dupree Norrington Jr. I was born on the 15th of January in the year 1999.
-I'm 22 :) I started messing with code when I was younger making bot scripts and things on online games I use to play like ClubPenguin.
-I'm currently learning html,css, javascript, and much more. Coding is so exciting and i'm glad im taking this leap into the field.
-My older sister conviced me to enroll in these courses and, I know I wont regret my choice!
-I've been thinking a lot and the company I would want to work for the most is Microsoft!</p>
-    </div> 
-    </center>
-</div>
+    <section className='contact-containter'>
+      <h1 data-testid="h1tag">Contact me</h1>
+      <form id="contact-form" onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input type="text" name="name" defaultValue={name} onBlur={handleChange} />
+        </div>
+        <div>
+          <label htmlFor="email">Email address:</label>
+          <input type="email" name="email" defaultValue={email} onBlur={handleChange} />
+        </div>
+        <div>
+          <label htmlFor="message">Message:</label>
+          <textarea name="message" rows="5" defaultValue={message} onBlur={handleChange} />
+        </div>
+        {errorMessage && (
+          <div>
+            <p className="error-text">{errorMessage}</p>
+          </div>
+        )}
+        <button data-testid="button" type="submit">Submit</button>
+      </form>
+    </section>
 )
 
 }
--- src/components/pages/Resume/index.js
+++ src/components/pages/Resume/index.js
@@ -1,5 +1,5 @@
 import react from "react";
-
+import giveaway from "../../../assets/giveaway.png"
 
 
 
@@ -7,18 +7,11 @@ import react from "react";
 
 function Resume(){
     return(
-        <div className="about-container">
-        <h1>Resume</h1>
-        <center>
-       <div className="about-p">
-        <p> My my full name is, Jovan Dupree Norrington Jr. I was born on the 15th of January in the year 1999.
-I'm 22 :) I started messing with code when I was younger making bot scripts and things on online games I use to play like ClubPenguin.
-I'm currently learning html,css, javascript, and much more. Coding is so exciting and i'm glad im taking this leap into the field.
-My older sister conviced me to enroll in these courses and, I know I wont regret my choice!
-I've been thinking a lot and the company I would want to work for the most is Microsoft!</p>
-        </div> 
-        </center>
-    </div>
+        <section>
+            <div className="resume-container">
+               <p>For a copy of my Resume </p> <a href={giveaway} download>Click Here</a>
+            </div>
+        </section>
     )
 }
 
